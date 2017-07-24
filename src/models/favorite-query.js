1a0d5e6df5da267162b3b4668e819765252b81c6
aherlihy <anna.herlihy@10gen.com>
2017-07-24 17:19:27 +0200

    Comment out StorageMixin for react-storybook

--- src/models/favorite-query.js
+++ src/models/favorite-query.js
@@ -1,17 +1,17 @@
 const Query = require('./query');
-const electronApp = require('electron').remote.app;
-const storageMixin = require('storage-mixin');
+// const electronApp = require('electron').remote.app;
+// const storageMixin = require('storage-mixin');
 
 /**
  * A model that represents a favorite MongoDB query.
  */
-const FavoriteQuery = Query.extend(storageMixin, {
+const FavoriteQuery = Query.extend({// storageMixin, {
   idAttribute: '_id',
   namespace: 'FavoriteQueries',
-  storage: {
-    backend: 'local',
-    appName: electronApp.getName()
-  },
+  // storage: {
+  //   backend: 'local',
+  //   appName: electronApp.getName()
+  // },
   props: {
     /**
      * The query name.
--- src/models/recent-query.js
+++ src/models/recent-query.js
@@ -1,17 +1,17 @@
 const Query = require('./query');
-const electronApp = require('electron').remote.app;
-const storageMixin = require('storage-mixin');
+// const electronApp = require('electron').remote.app;
+// const storageMixin = require('storage-mixin');
 
 /**
  * A model that represents a recent MongoDB query.
  */
-const RecentQuery = Query.extend(storageMixin, {
+const RecentQuery = Query.extend({// storageMixin, {
   idAttribute: '_id',
-  namespace: 'RecentQueries',
-  storage: {
-    backend: 'local',
-    appName: electronApp.getName()
-  }
+  namespace: 'RecentQueries'
+  // ,storage: {
+  //   backend: 'local',
+  //   appName: electronApp.getName()
+  // }
 });
 
 module.exports = RecentQuery;
