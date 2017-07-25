6bd5921723fe1cf4f35d71697f9ef8f95f008bcb
aherlihy <anna.herlihy@10gen.com>
2017-07-25 11:28:05 +0200

    Collapsing working

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const RecentQuery = require('./lib/models/recent-query');
 const FavoriteQuery = require('./lib/models/favorite-query');
 const RecentQueryCollection = require('./lib/models/recent-query-collection');
 const FavoriteQueryCollection = require('./lib/models/favorite-query-collection');
+const QueryHistoryActions = require('./lib/actions');
 
 /**
  * Activate all the components in the Query History package.
@@ -11,6 +12,7 @@ const FavoriteQueryCollection = require('./lib/models/favorite-query-collection'
 function activate(appRegistry) {
   appRegistry.registerStore('QueryHistory.Store', QueryHistoryStore);
   appRegistry.registerComponent('QueryHistory.Component', QueryHistoryComponent);
+  appRegistry.registerAction('QueryHistory.Actions', QueryHistoryActions);
 }
 
 /**
@@ -18,7 +20,8 @@ function activate(appRegistry) {
  */
 function deactivate(appRegistry) {
   appRegistry.deregisterStore('QueryHistory.Store');
-  appRegistry.deregisterComponent('QueryHistory.Component', QueryHistoryComponent);
+  appRegistry.deregisterComponent('QueryHistory.Component');
+  appRegistry.deregisterAction('QueryHistory.Actions');
 }
 
 module.exports = QueryHistoryComponent;
