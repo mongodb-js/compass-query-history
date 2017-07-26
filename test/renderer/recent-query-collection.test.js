ae5ee397183b7a2ecd13f84fbd4d563917e1f061
Durran Jordan <durran@gmail.com>
2017-07-26 13:14:38 +0200

    Fixing tests

--- test/renderer/recent-query-collection.test.js
+++ test/renderer/recent-query-collection.test.js
@@ -4,7 +4,7 @@ const { RecentQuery, RecentQueryCollection } = require('../../');
 describe('RecentQueryCollection', () => {
   describe('#add', () => {
     const collection = new RecentQueryCollection();
-    const query = new RecentQuery({ filter: { name: 'test' } });
+    const query = new RecentQuery({ filter: "{ name: 'test' }" });
 
     before(() => {
       collection.add(query);
@@ -20,7 +20,7 @@ describe('RecentQueryCollection', () => {
   });
 
   describe('#new', () => {
-    const query = new RecentQuery({ filter: { name: 'test' } });
+    const query = new RecentQuery({ filter: "{ name: 'test' }" });
     const collection = new RecentQueryCollection([ query ]);
 
     it('adds the query to the collection', () => {
@@ -35,13 +35,13 @@ describe('RecentQueryCollection', () => {
   context('when the collection has multiples', () => {
     const older = new Date('2014-01-01');
     const newer = new Date();
-    const queryOne = new RecentQuery({ _lastExecuted: older, filter: {'one': 1} });
-    const queryTwo = new RecentQuery({ _lastExecuted: newer, filter: {'two': 1} });
+    const queryOne = new RecentQuery({ _lastExecuted: older, filter: '{ one: 1 }' });
+    const queryTwo = new RecentQuery({ _lastExecuted: newer, filter: '{ two: 1 }' });
     const collection = new RecentQueryCollection([ queryOne, queryTwo ]);
 
     it('sorts the collection by _dateSaved', () => {
-      expect(collection.models[0].filter).to.deep.equal({'two': 1});
-      expect(collection.models[1].filter).to.deep.equal({'one': 1});
+      expect(collection.models[0].filter).to.deep.equal('{ two: 1 }');
+      expect(collection.models[1].filter).to.deep.equal('{ one: 1 }');
     });
   });
 });
